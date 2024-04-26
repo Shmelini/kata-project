1e71f3d632b7c9c16ffb7e32772b190a1969a663
Artem Bondarchuk <arhipilagys@gmail.com>
2024-04-26 23:16:59 +1100

    made-it-better(i think?)

--- js/show-more.js
+++ js/show-more.js
@@ -1,21 +1,23 @@
 function hideExtraBrands() {
-    const brands = document.querySelectorAll('.swiper-slide')
+    const brands = document.querySelectorAll('.swiper-slide');
 
     if (window.innerWidth < 1024) {
         for (let i = 6; i < brands.length; i++) {
-            brands[i].classList.add('hidden')
+            brands[i].classList.add('hidden');
+            setTimeout(() => { brands[i].style.display = 'none' }, 100)
         }
     }
     else if (window.innerWidth >= 1024) {
         for (let i = 8; i < brands.length; i++) {
-            brands[i].classList.add('hidden')
+            brands[i].classList.add('hidden');
+            setTimeout(() => { brands[i].style.display = 'none' }, 100)
         }
     }
 }
 
-document.addEventListener('DOMContentLoaded', hideExtraBrands)
+document.addEventListener('DOMContentLoaded', hideExtraBrands);
 
-const showMoreButton = document.querySelector('.button-spoiler')
+const showMoreButton = document.querySelector('.button-spoiler');
 
 showMoreButton.addEventListener('click', () => {
     const hiddenBrands = document.querySelectorAll('.swiper-brands__swiper-wrapper .hidden')
@@ -25,12 +27,13 @@ showMoreButton.addEventListener('click', () => {
 
     if (showMoreButton.classList.contains('button-spoiler--opened')) {
         hiddenBrands.forEach((brand) => {
-            brand.classList.remove('hidden')
+            setTimeout(() => { brand.style.display = 'block' }, 1)
+            setTimeout(() => { brand.classList.remove('hidden') }, 10)
         })
-        showMoreButton.querySelector('.button-spoiler__text').textContent = 'Скрыть'
+        showMoreButton.querySelector('.button-spoiler__text').textContent = 'Скрыть';
     }
     else if (showMoreButton.classList.contains('button-spoiler--closed')) {
-        showMoreButton.querySelector('.button-spoiler__text').textContent = 'Показать ещё'
-        hideExtraBrands()
+        showMoreButton.querySelector('.button-spoiler__text').textContent = 'Показать ещё';
+        hideExtraBrands();
     }
 })
